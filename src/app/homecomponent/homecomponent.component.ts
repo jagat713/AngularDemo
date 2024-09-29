82559a31047567dad9c44e5f2043beac3f99eec8
Jagat <beherajagat713@gmail.com>
2024-09-29 23:44:03 +0530

    newhomecomponent

---
+++ src/app/homecomponent/homecomponent.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homecomponent',
+  standalone: true,
+  imports: [],
+  templateUrl: './homecomponent.component.html',
+  styleUrl: './homecomponent.component.css'
+})
+export class HomecomponentComponent {
+
+}
